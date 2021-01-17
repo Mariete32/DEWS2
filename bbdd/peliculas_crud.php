e78c9f11fdb71a8f6aced3ae13b4aac23d4d6726
Mario Garcia Reyes <mgkm10@gmail.com>
2021-01-17 13:08:28 +0100

    se implementa el guarado con exito o no ha sido guardado con exito en peliculas_form.php

--- bbdd/peliculas_crud.php
+++ bbdd/peliculas_crud.php
@@ -2,40 +2,45 @@
 require_once './classes/peliculas.php';
 require_once './lib/database.php';
 
-class CrudPeliculas{
-function __construct(){}
+class CrudPeliculas
+{
+    public function __construct()
+    {}
 //funcion que nos devuelve la clase pelicula con sus datos
-public function obtenerPelicula($id){
-    
-    $conexion = database::conexion();
-    $consulta = "SELECT * FROM peliculas WHERE id= $id";
-    $consultaPreparada = $conexion->prepare($consulta);
-    $consultaPreparada->execute();
-    $resultado=$consultaPreparada->fetchAll(PDO::FETCH_ASSOC);
-foreach ($resultado as $value) {
-    $titulo=$value["titulo"];
-    $anyo=$value["anyo"];
-    $duracion=$value["duracion"];
-    $pelicula=new pelicula($id,$anyo,$duracion,$titulo);
-}
-return $pelicula;
-}
+    public function obtenerPelicula($id)
+    {
+        $conexion = database::conexion();
+        $consulta = "SELECT * FROM peliculas WHERE id= $id";
+        $consultaPreparada = $conexion->prepare($consulta);
+        $consultaPreparada->execute();
+        $resultado = $consultaPreparada->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($resultado as $value) {
+            $titulo = $value["titulo"];
+            $anyo = $value["anyo"];
+            $duracion = $value["duracion"];
+            $pelicula = new pelicula($id, $anyo, $duracion, $titulo);
+        }
+        return $pelicula;
+    }
 
 //funcion que actualiza los valores en la base de datos
-public static function editarPelicula($id,$titulo,$anyo,$duracion){
-    $conexion = database::conexion();
-    $actualizacion="UPDATE peliculas SET titulo=:titulo, duracion=:duracion, anyo=:anyo WHERE id=$id";
-    $consultaPreparada=$conexion->prepare($actualizacion);
-    $consultaPreparada->bindparam(':titulo', $titulo);
-    $consultaPreparada->bindparam(':anyo', $anyo);
-    $consultaPreparada->bindparam(':duracion', $duracion);
-    $consultaPreparada->execute();
+    public static function editarPelicula($id, $titulo, $anyo, $duracion)
+    {
+        try {
+            $conexion = database::conexion();
+            $actualizacion = "UPDATE peliculas SET titulo=:titulo, duracion=:duracion, anyo=:anyo WHERE id=$id";
+            $consultaPreparada = $conexion->prepare($actualizacion);
+            $consultaPreparada->bindparam(':titulo', $titulo);
+            $consultaPreparada->bindparam(':anyo', $anyo);
+            $consultaPreparada->bindparam(':duracion', $duracion);
+            $consultaPreparada->execute();
+            $exito = 1;
+            return $exito;
+        } catch (exception $e) {
+            $exito = 0;
+            return $exito;
+        }
+
+    }
+
 }
-
-
-
-
-
-
-}
-?>
--- peliculas_form.php
+++ peliculas_form.php
@@ -16,44 +16,56 @@ session_start();
 </head>
 
 <body>
-<?php
+    <?php
 if (isset($_POST["titulo"]) & isset($_POST["anyo"]) & isset($_POST["duracion"])) {
     //si insertamos datos, actualizamos los datos de esa pelicula
-    $crudPelicula=new CrudPeliculas();
-    $crudPelicula::editarPelicula($_GET["id"],$_POST["titulo"],$_POST["anyo"],$_POST["duracion"]);
+    $crudPelicula = new CrudPeliculas();
+    $exito=$crudPelicula::editarPelicula($_GET["id"], $_POST["titulo"], $_POST["anyo"], $_POST["duracion"]);
+    if ($exito==1) {
+        echo '<div class="alert alert-success" role="alert">';
+        echo "<h2>La película ha sido guardada con éxito</h2></div>";
+        echo '<a href="películas.php">volver al inicio</a>';
+    } else {
+        echo '<div class="alert alert-danger" role="alert">';
+        echo "<h2>La película no ha sido guardada con éxito</h2></div>";
+        echo '<a href="peliculas.php">volver al inicio</a>';
+    }
 
-}
-$id=$_GET["id"];
-$crudPelicula=new CrudPeliculas();
-$pelicula=$crudPelicula->obtenerPelicula($id);
-$titulo=$pelicula->get_titulo();
-$anyo=$pelicula->get_anyo();
-$duracion=$pelicula->get_duracion();
-
-
+}else{
+$id = $_GET["id"];
+$crudPelicula = new CrudPeliculas();
+$pelicula = $crudPelicula->obtenerPelicula($id);
+$titulo = $pelicula->get_titulo();
+$anyo = $pelicula->get_anyo();
+$duracion = $pelicula->get_duracion();
 
 ?>
     <div class="alert alert-secondary d-flex">
         <a href="./peliculas.php" class="btn btn-dark">Películas</a>&nbsp;&nbsp;
     </div>
     <div class="container">
-    <form action="peliculas_form.php?id=<?php echo $id?>" method="POST">
-                <p>
-                    <label for="titulo">Titulo: </label>
-                    <input type="text" name="titulo" value="<?php echo $titulo ?>">
-                </p>
-                <p>
-                    <label for="anyo">Año</label>
-                    <input type="text" name="anyo" value="<?php echo $anyo?>">
-                </p>
-                <p>
-                    <label for="duracion">duracion</label>
-                    <input type="text" name="duracion" value="<?php echo $duracion?>">
-                </p>
-                <p> <input type="submit" value="Guardar"> </p>
-            </form>
+        <h1>Edición de películas</h1>
+        <form action="peliculas_form.php?id=<?php echo $id ?>" method="POST">
+            <p>
+                <label for="titulo">Titulo: </label>
+                <input type="text" name="titulo" value="<?php echo $titulo ?>">
+            </p>
+            <p>
+                <label for="anyo">Año</label>
+                <input type="text" name="anyo" value="<?php echo $anyo ?>">
+            </p>
+            <p>
+                <label for="duracion">duracion</label>
+                <input type="text" name="duracion" value="<?php echo $duracion ?>">
+                <input type="hidden" name="guardado" value="ok">
+            </p>
+            <p> <input type="submit" value="Guardar"> </p>
+        </form>
     </div>
 
 </body>
+<?php
+}
+?>
 
 </html>
